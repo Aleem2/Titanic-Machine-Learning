34dac7c0bb7c7b8af891b4557f29cbd040856f69
Aleem2 <43530611+Aleem2@users.noreply.github.com>
2023-09-25 17:03:24 +1000

    Update titanic2.py
    running stats on survived column

--- titanic2.py
+++ titanic2.py
@@ -13,6 +13,8 @@ print(train.iloc[3,:])
 print(train.iloc[4,:3])
 print(train.iloc[:,2])
 print(train.loc[:,['Pclass','Age']])
-#Suggestion to my self is stick to loc and iloc to refer to specific column and rows.
+# Suggestion to my self is stick to loc and iloc to refer to specific column and rows.
 print(train.loc[:,['PassengerId','Survived']])
-
+# Now I want to see stats of of the survived column. How can I do so?
+# One approach is to create a sub data frame and run describe on it. Infact without creating a variable. 
+print((train.loc[:,['PassengerId','Survived']]).describe())
