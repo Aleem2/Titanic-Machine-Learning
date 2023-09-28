d7d5497ed453b067844c826481fa4fb68e7bce36
Aleem Uddin <md.aleem@gmail.com>
2023-09-28 11:55:37 +0000

    converted ipynb to .py file

--- titanic2.py
+++ titanic2.py
@@ -20,3 +20,56 @@ print(train.loc[:,['PassengerId','Survived']])
 print((train.loc[:,['PassengerId','Survived']]).describe())
 #testing out the fetch git command.
 #tested successfully.
+
+#Collected and clean subset of the data for training
+train_data=train.loc[:,['PassengerId','Sex','Age','Pclass','Survived']]
+train_data['Age'].interpolate(str='linear',inplace=True)
+train_data.describe()
+test=pd.read_csv('test.csv')
+test.describe()
+test_data=test.loc[:,['PassengerId','Sex','Age','Pclass']]
+# to note that Survived column is missing
+test_data['Age'].interpolate(str='linear',inplace=True)
+test_data.describe()
+#-----------------------------------
+#Using Scikit Learn to train models
+
+#Creating the training dataset
+# feature matrix
+X = train_data[['PassengerId','Sex','Age','Pclass']]
+X.loc[:,'Sex'] = X.loc[:,'Sex'].apply(lambda x: 1 if x == 'male' else 0)
+# result matrix
+y = train_data['Survived']
+y.describe()
+
+#
+
+# example code from tutorial https://scikit-learn.org/stable/getting_started.html
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+# create a pipeline object
+pipe = make_pipeline(
+    StandardScaler(),
+    LogisticRegression()
+)
+# load the iris dataset and split it into train and test sets
+#X, y = load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+#print(y_train)
+# fit the whole pipeline
+pipe.fit(X_train, y_train)
+# we can now use it like any other estimator
+accuracy_score(pipe.predict(X_test), y_test)
+
+#----------------------------------------------------------------------------------------
+#Model evaluation - cross validation by splitting the data using different approaches.
+from sklearn.model_selection import cross_validate
+result = cross_validate(pipe, X, y)
+result['test_score']
+
+
+
